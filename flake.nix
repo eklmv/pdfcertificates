c78a702ce82b086c38683e7d7c58d864704fad91
Evgenii Klimov <eklmv.mail@gmail.com>
2023-12-28 16:56:22 +0300

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Go development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      goV = 21;
+      overlays = [
+        (_: prev: {
+          go = prev."go_1_${toString goV}";
+        })
+      ];
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default =
+          pkgs.mkShell { packages = with pkgs; [ go gotools gopls ]; };
+      });
+    };
+}
